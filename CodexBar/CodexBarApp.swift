200bbe1a2895f2de5c9582bf792292db11cb991b
Kexin <Kexin_0@outlook.com>
2026-09-19 12:24:12 +0800

    Initial Commit

---
+++ CodexBar/CodexBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexBarApp.swift
+//  CodexBar
+//
+//  Created by 可可可心 on 9/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CodexBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
